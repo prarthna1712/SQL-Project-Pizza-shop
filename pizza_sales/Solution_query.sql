bb42e11fba676f69e92f0fd51cee68d3312a7bf8
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-11-21 19:18:54 +0530

    init

--- pizza_sales/Solution_query.sql
+++ pizza_sales/Solution_query.sql
@@ -1,10 +1,19 @@
-use pizzavizza;
 -- 1. Retrieve the total number of orders placed.
 select count(order_id) as order_id
 from orders ;
 
 -- 2. Calculate the total revenue generated from pizza sales.
-select sum(od.quantity * p.price) as Reveue
+select round(sum(od.quantity * p.price)) as Reveue
 from order_details od
 join pizzas p
 on od.pizza_id = p.pizza_id;
+
+-- Identify the highest-priced pizza.
+select p.price, pt.name
+from pizzas p 
+join pizza_types pt
+on p.pizza_type_id = pt.pizza_type_id
+order by price desc
+limit 1;
+
+

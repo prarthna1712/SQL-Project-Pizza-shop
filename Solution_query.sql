359bd996c56119d5806215be92b423d37a897b96
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-11-22 16:46:29 +0530

    init

--- Solution_query.sql
+++ Solution_query.sql
@@ -24,3 +24,14 @@ on od.pizza_id = p.pizza_id
 group by p.size
 order by total_order desc 
 limit 1;
+
+-- 5. List the top 5 most ordered pizza types along with their quantities.
+select pt.name, sum(od.quantity) as total 
+from pizza_types pt
+join pizzas p 
+on pt.pizza_type_id = p.pizza_type_id
+join order_details od
+on od.pizza_id = p.pizza_id
+group by pt.name
+order by total desc
+limit 5;

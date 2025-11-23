c7fd9a182a9f6380fab97b1eed5a7c797d9e68a4
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-11-23 21:41:59 +0530

    init

--- Solution_query.sql
+++ Solution_query.sql
@@ -44,3 +44,21 @@ on pt.pizza_type_id = p.pizza_type_id
 join order_details od
 on od.pizza_id = p.pizza_id
 group by category;
+
+-- 7. Determine the distribution of orders by hour of the day.
+select hour(order_time) as hours, count(order_id) as total_orders
+from orders
+group by hours;
+
+-- 8. Join relevant tables to find the category-wise distribution of pizzas.
+select pt.category, count(p.pizza_id) as total
+from pizza_types pt 
+join pizzas p 
+on pt.pizza_type_id = p.pizza_type_id
+group by category;
+-- or -->
+select category, count(name) from pizza_types
+group by category;
+
+-- 9. Group the orders by date and calculate the average number of pizzas ordered per day.
+

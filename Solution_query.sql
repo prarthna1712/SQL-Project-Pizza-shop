0c8d6fc3e38e37268ce82f0e6c30f44b3530bcd1
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-11-23 13:44:30 +0530

    init

--- Solution_query.sql
+++ Solution_query.sql
@@ -35,3 +35,12 @@ on od.pizza_id = p.pizza_id
 group by pt.name
 order by total desc
 limit 5;
+
+-- 6. Join the necessary tables to find the total quantity of each pizza category ordered.
+select sum(od.quantity) as quantity, pt.category 
+from pizza_types pt
+join pizzas p
+on pt.pizza_type_id = p.pizza_type_id
+join order_details od
+on od.pizza_id = p.pizza_id
+group by category;
